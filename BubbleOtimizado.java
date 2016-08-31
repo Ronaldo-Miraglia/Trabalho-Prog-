54c9c3e1c52b474b05479721854173e427aef1fc
Ronaldo-Miraglia <ronaldopraia@hotmail.com>
2016-08-31 14:50:35 -0300

    Add files via upload

---
+++ BubbleOtimizado.java
@@ -0,0 +1,20 @@
+public class BubbleOtimizado extends Pai {
+	
+	public void sort(int[] v) {
+		for(int i = 0;i<v.length; i++){
+			int troca = 0;
+			for(int j = 0; j < v.length; j++){
+				if(j+1 == v.length)
+					break;
+				else if(v[j] > v[j+1]){
+					swap(v,j,j+1);
+					troca++;
+				}
+				
+			}
+			if(troca == 0)
+				break;
+		}
+		
+	}
+}
